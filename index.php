4d023fc7a52c606ec4f90192bf9fdc75c4c54f63
Tarun Krishna <112946269+Mtarunkrishna@users.noreply.github.com>
2023-01-10 15:20:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,7 @@
 <?php
 
-<!DOCTYPE html>
-
-<html>
-
-    <head>
-
-        <title>Example</title>
-
-    </head>
-
-    <body>
-
-        <p>Hello, world!</p>
-
-    </body>
-
-</html>
-    
-    header("Location: https://github.com/", true, 302)
+ header('Location: https://www.google.com/')
     
     exit;
 
 ?>
-
